ba491a9e337e457f63da26eb4e30fb7581c47293
DESKTOP-DH1O5F3\PC <dzenis.brkan@outlook.com>
2024-06-27 00:04:04 +0200

    Update Program

--- Client_App/Client_App/Program.cs
+++ Client_App/Client_App/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IClientService, ClientService>();
+builder.Services.AddScoped<IClientService, ClientService>();
 
 var app = builder.Build();
 
